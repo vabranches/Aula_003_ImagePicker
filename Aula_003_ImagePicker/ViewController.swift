755f5d50d14079f223f4915a5c376b0f4ef941ab
Swift <swift@WozMac8.local>
2017-01-18 22:29:01 -0200

    inicial

--- Aula_003_ImagePicker/ViewController.swift
+++ Aula_003_ImagePicker/ViewController.swift
@@ -1,23 +1,52 @@
-//
-//  ViewController.swift
-//  Aula_003_ImagePicker
-//
-//  Created by Swift on 18/01/17.
-//  Copyright © 2017 Swift. All rights reserved.
-//
+/*
+ Não esquecer de adicionar a propriedade 
+ - Privacy - Photo Library Usage Description
+ no Info.plist
+ 
+ 
+ */
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    //MARK: OUTLETS
+    @IBOutlet weak var minhaImageView: UIImageView!
+    
+    //MARK: Propriedades
+    let imagePicker = UIImagePickerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        //MARK: Delegate
+        imagePicker.delegate = self
+        
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: ACTIONS
+    @IBAction func carregarImagem(_ sender: UIButton) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .savedPhotosAlbum
+        
+        print("PRINT: Acessou")
+        
+        present(imagePicker, animated: true)
+    }
+    
+    //MARK: PICKER DELEGATE
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let imagemEscolhida = info[UIImagePickerControllerOriginalImage] as! UIImage
+        minhaImageView.image = imagemEscolhida
+        picker.dismiss(animated: true)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        print("PRINT: Cancelou")
+        minhaImageView.image = nil
+        picker.dismiss(animated: true)
+        
     }
 
 
